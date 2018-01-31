f12c5295d71842d539e371b42cc860191b70f39f
Namith Sanal <nsanal2@illinois.edu>
2018-01-31 18:11:37 -0400

    finished i guess

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,7 +34,19 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            maze.turnLeft();
+            if (maze.canMove()){
+                maze.move();
+            }
+            else{
+                while(!maze.canMove()) {
+                    maze.turnRight();
+                }
+                maze.move();
+            }
+            if(maze.isFinished()){
+                break;
+            }
         }
 
         if (maze.isFinished()) {
